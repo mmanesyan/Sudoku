05d9f8629194cf66ea2a71a8d98bc64e8701b74c
VazgenBablumyan <vazgeb@gmail.com>
2023-04-16 17:34:17 +0400

    Feat(sudoku.js, index.html)

---
+++ sudoku.js
@@ -0,0 +1,256 @@
+    let numbers = [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
+let newNumbers = []
+function sort(array){
+    for(let i = 0; i<array.length;i++){
+        let number = array[i]
+        number.sort((a) => Math.random()*a-(number.length-1)/2)
+}
+}
+let answer = false
+function s(){
+    
+    sort(numbers)
+    while(answer){
+
+   
+        
+    switch (numbers[0][0]){
+        case numbers[1][0]:
+            sort(numbers)
+            break
+            case numbers[2][0]:
+            sort(numbers)
+            break
+            case numbers[3][0]:
+            sort(numbers)
+            break
+            case numbers[4][0]:
+            sort(numbers)
+            break
+            case numbers[5][0]:
+            sort(numbers)
+            break
+            case numbers[6][0]:
+            sort(numbers)
+            break
+            case numbers[7][0]:
+            sort(numbers)
+            break
+            case numbers[8][0]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][1]){
+        case numbers[1][1]:
+            sort(numbers)
+            break
+            case numbers[2][1]:
+            sort(numbers)
+            break
+            case numbers[3][1]:
+            sort(numbers)
+            break
+            case numbers[4][1]:
+            sort(numbers)
+            break
+            case numbers[5][1]:
+            sort(numbers)
+            break
+            case numbers[6][1]:
+            sort(numbers)
+            break
+            case numbers[7][1]:
+            sort(numbers)
+            break
+            case numbers[8][1]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][2]){
+        case numbers[1][2]:
+            sort(numbers)
+            break
+            case numbers[2][2]:
+            sort(numbers)
+            break
+            case numbers[3][2]:
+            sort(numbers)
+            break
+            case numbers[4][2]:
+            sort(numbers)
+            break
+            case numbers[5][2]:
+            sort(numbers)
+            break
+            case numbers[6][2]:
+            sort(numbers)
+            break
+            case numbers[7][2]:
+            sort(numbers)
+            break
+            case numbers[8][2]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][3]){
+        case numbers[1][3]:
+            sort(numbers)
+            break
+            case numbers[2][3]:
+            sort(numbers)
+            break
+            case numbers[3][3]:
+            sort(numbers)
+            break
+            case numbers[4][3]:
+            sort(numbers)
+            break
+            case numbers[5][3]:
+            sort(numbers)
+            break
+            case numbers[6][3]:
+            sort(numbers)
+            break
+            case numbers[7][3]:
+            sort(numbers)
+            break
+            case numbers[8][3]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][4]){
+        case numbers[1][4]:
+            sort(numbers)
+            break
+            case numbers[2][4]:
+            sort(numbers)
+            break
+            case numbers[3][4]:
+            sort(numbers)
+            break
+            case numbers[4][4]:
+            sort(numbers)
+            break
+            case numbers[5][4]:
+            sort(numbers)
+            break
+            case numbers[6][4]:
+            sort(numbers)
+            break
+            case numbers[7][4]:
+            sort(numbers)
+            break
+            case numbers[8][4]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][5]){
+        case numbers[1][0]:
+            sort(numbers)
+            break
+            case numbers[2][5]:
+            sort(numbers)
+            break
+            case numbers[3][5]:
+            sort(numbers)
+            break
+            case numbers[4][5]:
+            sort(numbers)
+            break
+            case numbers[5][5]:
+            sort(numbers)
+            break
+            case numbers[6][5]:
+            sort(numbers)
+            break
+            case numbers[7][5]:
+            sort(numbers)
+            break
+            case numbers[8][5]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][6]){
+        case numbers[1][6]:
+            sort(numbers)
+            break
+            case numbers[2][6]:
+            sort(numbers)
+            break
+            case numbers[3][6]:
+            sort(numbers)
+            break
+            case numbers[4][6]:
+            sort(numbers)
+            break
+            case numbers[5][6]:
+            sort(numbers)
+            break
+            case numbers[6][6]:
+            sort(numbers)
+            break
+            case numbers[7][6]:
+            sort(numbers)
+            break
+            case numbers[8][6]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][7]){
+        case numbers[1][7]:
+            sort(numbers)
+            break
+            case numbers[2][7]:
+            sort(numbers)
+            break
+            case numbers[3][7]:
+            sort(numbers)
+            break
+            case numbers[4][7]:
+            sort(numbers)
+            break
+            case numbers[5][7]:
+            sort(numbers)
+            break
+            case numbers[6][7]:
+            sort(numbers)
+            break
+            case numbers[7][7]:
+            sort(numbers)
+            break
+            case numbers[8][7]:
+            sort(numbers)
+            break
+    }
+    switch (numbers[0][8]){
+        case numbers[1][8]:
+            sort(numbers)
+            break
+            case numbers[2][8]:
+            sort(numbers)
+            break
+            case numbers[3][8]:
+            sort(numbers)
+            break
+            case numbers[4][8]:
+            sort(numbers)
+            break
+            case numbers[5][8]:
+            sort(numbers)
+            break
+            case numbers[6][8]:
+            sort(numbers)
+            break
+            case numbers[7][8]:
+            sort(numbers)
+            break
+            case numbers[8][8]:
+            sort(numbers)
+            break
+    }
+     answer = true
+    }
+    return numbers
+}
+
+console.log(s())
